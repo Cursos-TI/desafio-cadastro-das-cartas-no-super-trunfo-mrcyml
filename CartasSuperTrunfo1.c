da7e01316a2837b1411e475f2cf4bed28e119062
mrcyml <marcelaborges038@gmail.com>
2025-02-16 17:40:30 +0000

    atualizacao e adicao do printf

---
+++ CartasSuperTrunfo1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+    int main() {
+
+        double populacao;
+        float area;
+        int pib, turisticos;
+        int estados, cidades;
+        char pais[20];
+
+
+        printf("%")
+
+
+
+
+    }
