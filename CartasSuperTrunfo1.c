1a39b8c4d510efc4afb50f95ca92a8166bf539d4
mrcyml <marcelaborges038@gmail.com>
2025-02-19 18:14:47 +0000

    acicionando os resultados da carta

--- CartasSuperTrunfo1.c
+++ CartasSuperTrunfo1.c
@@ -1,16 +1,43 @@
 #include <stdio.h>
     int main() {
 
-        double populacao;
-        float area;
-        int pib, turisticos;
-        int estados, cidades;
-        char pais[20];
+        float area, pib;
+        double populacao, cidades;
+        int estados, turisticos;
+        char pais[80];
+    
+//cadastro da carta.
+
+        printf("Qual o país da sua carta? \n");
+        scanf("%s", &pais);
+
+        printf("Quantos Estados? \n");
+        scanf("%d", &estados);
+
+        printf("Quantas cidades? \n");
+        scanf("%f", &cidades);
+
+        printf("População? \n");
+        scanf("%f" , &populacao);
+
+        printf("Quantos pontos turísticos? \n");
+        scanf("%d", &turisticos);
+
+        printf("PIB? \n");
+        scanf("%f", &pib);
 
 
-        printf("%")
+        //Exibindo o resultado
+
+        printf("Sua Carta:\n");
+        printf("País: %s\n", pais);
+        printf("Estados: %d\n", estados);
+        printf("Cidades: %f mil\n", cidades);
+        printf("População: %f milhões\n", populacao);
+        printf("Pontos Turísticos: %d\n", turisticos);
+        printf("PIB: %f trilhões USD\n", pib);
 
 
-
+        return 0;
 
     }
